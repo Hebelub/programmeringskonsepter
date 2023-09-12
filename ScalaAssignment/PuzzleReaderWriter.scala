fcca76a6a33a1e87352b3a3755ffa353c1400639
Gabriel Løsnesløkken <gabriell@uia.no>
2023-09-12 09:57:07 +0200

    Change folder

---
+++ ScalaAssignment/PuzzleReaderWriter.scala
@@ -0,0 +1,103 @@
+import scala.io.Source
+import scala.util.control.Breaks._
+
+object PuzzleReaderWriter {
+
+  def stringToGridCell(cell: Char): Char = cell match {
+    case '*' => '*'
+    case 'o' => 'o'
+    case '_' => '.'
+    case _ => throw new Exception("Unknown cell type")
+  }
+
+  def readPuzzlesFromFile(filePath: String): List[Puzzle.Grid] = {
+    val lines = Source.fromFile(filePath).getLines().toList
+    var puzzles = List[Puzzle.Grid]()
+    var i = 1  // Skip the first line which contains the number of puzzles
+
+    while (i < lines.size) {
+      val sizeLine = lines(i)
+      val Array(cols, rows) = sizeLine.split(" ")(1).split("x").map(_.toInt)
+      i += 1
+
+      var grid = Puzzle.createGrid(rows, cols)  // Initialize an empty grid
+      var incompleteGrid = false
+
+      breakable {
+        for (r <- 0 until rows) {
+          if (i + r < lines.size) {
+            val rowCells = lines(i + r).replaceAll(" ", "").map(stringToGridCell)
+            for (c <- 0 until cols) {
+              grid = Cell.setCell(grid, (r * 2, c * 2, rowCells(c)))  // Update the grid
+            }
+          } else {
+            // Handle the error or break the loop
+            println(s"Warning: Not enough lines to read a full ${rows}x${cols} grid.")
+            incompleteGrid = true
+            // Exit the for-loop
+            break
+          }
+        }
+      }
+
+      if (!incompleteGrid) {
+        puzzles = puzzles :+ grid  // Add the grid to the list of puzzles
+        i += rows
+      }
+    }
+
+    puzzles
+  }
+
+  def getSymbolFromCell(grid: Puzzle.Grid, cell: Cell.Cell): Char = {
+    val (rowIndex, colIndex, cellType) = cell
+
+    // Get the adjacent cells
+    val adjacentCells = List(
+      Cell.getRelativeCell(grid, cell, 0, 1)._3,
+      Cell.getRelativeCell(grid, cell, 1, 0)._3,
+      Cell.getRelativeCell(grid, cell, 0, -1)._3,
+      Cell.getRelativeCell(grid, cell, -1, 0)._3
+    )
+
+    // Pattern match to find the correct symbol
+    adjacentCells match {
+      case List(a, b, c, d) if List(a, b, c, d).count(_ == 'l') == 2 && cellType == '*' => '┼'
+      case List(_, a, _, b) if a == 'l' && b == 'l' && cellType == 'o' => '╡'
+      case List(a, _, b, _) if a == 'l' && b == 'l' && cellType == 'o' => '╨'
+      case List(a, b, _, _) if a == 'l' && b == 'l' => '┌'
+      case List(_, a, b, _) if a == 'l' && b == 'l' => '┐'
+      case List(_, _, a, b) if a == 'l' && b == 'l' => '┘'
+      case List(a, _, _, b) if a == 'l' && b == 'l' => '└'
+      case List(a, b, c, _) if a == 'l' && b == 'l' && c == 'l' => '├'
+      case List(_, a, b, c) if a == 'l' && b == 'l' && c == 'l' => '┤'
+      case List(a, _, b, c) if a == 'l' && b == 'l' && c == 'l' => '┴'
+      case List(a, b, _, c) if a == 'l' && b == 'l' && c == 'l' => '┬'
+      case List(a, b, c, d) if a == 'l' && b == 'l' && c == 'l' && d == 'l' => '┼'
+      case List(a, _, b, _) if a == 'l' && b == 'l' => '─'
+      case List(_, a, _, b) if a == 'l' && b == 'l' => '│'
+      case _ if cellType == '*' => '*'
+      case _ if cellType == 'o' => 'o'
+      case _ => ' '
+    }
+  }
+
+  def gridToString(grid: Puzzle.Grid): String = {
+    grid.zipWithIndex.flatMap { case (row, rowIndex) =>
+      if (rowIndex % 2 == 0) {  // Only process rows with even index
+        Some(
+          row.zipWithIndex.flatMap { case (cell, colIndex) =>
+            if (colIndex % 2 == 0) {  // Only process columns with even index
+              Some(getSymbolFromCell(grid, (rowIndex, colIndex, cell)))
+            } else {
+              None  // Skip cells with odd column index
+            }
+          }.mkString("")
+        )
+      } else {
+        None  // Skip rows with odd index
+      }
+    }.mkString("\n")
+  }
+
+}
---
+++ ScalaAssignment/PuzzleSolver.scala
@@ -0,0 +1,224 @@
+import scala.annotation.tailrec
+
+object PuzzleSolver {
+
+  def isCellSatisfied(grid: Puzzle.Grid, cell: Cell.Cell): Boolean = {
+    val (row, col, cellType) = cell
+    val lineType = Cell.determineLineType(grid, cell)
+    val connectedCells = Cell.getConnectedAdjacentNodes(grid, cell)
+
+    def areConnectedCellsStraight: Boolean = {
+      connectedCells.forall { connectedCell =>
+        Cell.determineLineType(grid, connectedCell) == Cell.Straight
+      }
+    }
+
+    def isAtLeastOneConnectedCellCorner: Boolean = {
+      connectedCells.exists { connectedCell =>
+        Cell.determineLineType(grid, connectedCell) == Cell.Corner
+      }
+    }
+
+    cellType match {
+      case '*' => lineType == Cell.Corner && areConnectedCellsStraight
+      case 'o' => (lineType == Cell.Straight || lineType == Cell.Corner) && isAtLeastOneConnectedCellCorner
+      case '.' => lineType == Cell.Straight || lineType == Cell.Corner || lineType == Cell.Empty
+      case _ => false // this covers other cases, which should not be part of the puzzle
+    }
+  }
+
+  def isSingleClosedLoop(grid: Puzzle.Grid): Boolean = {
+    val allLines = Puzzle.getCellsOfTypes(grid, List('l')).toSet
+    var visited = Set[Cell.Cell]()
+    var stack = List[Cell.Cell]()
+    
+    if (allLines.isEmpty) return false
+    
+    val firstLine = allLines.head
+    visited += firstLine
+    stack = firstLine :: stack
+    
+    while (stack.nonEmpty) {
+      val currentLine = stack.head
+      stack = stack.tail
+      
+      val allAdjacentLines = Cell.getConnectedLines(grid, currentLine)
+      val unvisitedAdjacentLines = allAdjacentLines.filter(!visited.contains(_))
+      
+      if (allAdjacentLines.size != 2) {
+        return false  // Not a single closed loop
+      }
+      
+      visited ++= unvisitedAdjacentLines
+      stack = unvisitedAdjacentLines ++ stack
+    }
+
+    visited == allLines
+  }
+
+  def isPuzzleComplete(grid: Puzzle.Grid): Boolean = {
+    val specialCells = Puzzle.getCellsOfTypes(grid, List('o', '*'))
+    val allCellsSatisfied = specialCells.forall(cell => isCellSatisfied(grid, cell))
+
+    allCellsSatisfied && isSingleClosedLoop(grid)
+  }
+
+  def applyRule(grid: Puzzle.Grid, cell: Cell.Cell, rule: Rule): Boolean = {
+    rule.conditions.forall { condition =>
+      val relativeCell = Cell.getRelativeCell(grid, cell, condition.deltaRow, condition.deltaCol)
+      relativeCell._3 == condition.cellType
+    }
+  }
+
+  def solveEdge(grid: Puzzle.Grid, cell: Cell.Cell, solveRules: List[Rule]): Cell.Cell = {
+    val (row, col, _) = cell
+    
+    val maybeRule = solveRules.find(rule => applyRule(grid, cell, rule))
+    
+    maybeRule match {
+      case Some(rule) => (row, col, rule.targetCellType)
+      case None => (row, col, ' ')
+    }
+  }
+
+  def applyRulesForOneCycle(grid: Puzzle.Grid, solveRules: List[Rule]): (Puzzle.Grid, Boolean) = {
+    val emptyEdges = Puzzle.getCellsOfTypes(grid, List(' '))
+    var newGrid = grid
+    var changed = false
+    
+    for (edge <- emptyEdges) {
+      val solvedEdge = solveEdge(newGrid, edge, solveRules)
+            if (solvedEdge._3 != ' ') {
+        newGrid = Cell.setCell(newGrid, solvedEdge)
+        changed = true
+      }
+    }
+    
+    (newGrid, changed)
+  }
+
+  def applyRulesUntilStable(grid: Puzzle.Grid, solveRules: List[Rule]): Puzzle.Grid = {
+    var currentGrid = grid
+    var lastUpdated: Option[Cell.Cell] = None
+    var done = false
+
+    // Apply rules until the grid is stable
+    while (!done) {
+      val (newGrid, changed) = applyRulesForOneCycle(currentGrid, solveRules)
+      currentGrid = newGrid
+      if (!changed) {
+        done = true
+      }
+    }
+
+    while (!done) {
+      val emptyEdges = Puzzle.getCellsOfTypes(currentGrid, List(' '))
+      
+      if (emptyEdges.isEmpty) {
+        done = true
+      } else {
+        var newLastUpdated: Option[Cell.Cell] = None
+        
+        for (edge <- emptyEdges) {
+          val solvedEdge = solveEdge(currentGrid, edge, solveRules)
+          if (solvedEdge._3 != ' ') {
+            currentGrid = Cell.setCell(currentGrid, solvedEdge)
+            newLastUpdated = Some(solvedEdge)
+          }
+        }
+        
+        if (newLastUpdated == lastUpdated) {
+          // No changes were made, and we are back to the last updated cell.
+          done = true
+        } else {
+          lastUpdated = newLastUpdated
+        }
+      }
+    }
+    
+    currentGrid
+  }
+
+  def getAllPossibleBranches(grid: Puzzle.Grid): List[Puzzle.Grid] = {
+    val emptyEdges = Puzzle.getCellsOfTypes(grid, List(' '))
+    
+    // Create two new grids for each empty edge
+    emptyEdges.flatMap { edge =>
+      val (row, col, _) = edge
+      // Add both 'l' and 'x' for each empty edge
+      List(
+        Cell.setCell(grid, (row, col, 'l')),
+        Cell.setCell(grid, (row, col, 'x'))
+      )
+    }
+  }
+
+  def solvePuzzle(grid: Puzzle.Grid, solveRules: List[Rule]): Option[Puzzle.Grid] = {
+
+    val stableGrid = applyRulesUntilStable(grid, solveRules)
+
+    if (isPuzzleComplete(stableGrid)) {
+      Some(stableGrid)
+    }
+    else {
+    
+      val emptyEdges = Puzzle.getCellsOfTypes(stableGrid, List(' '))
+      
+      if (emptyEdges.nonEmpty) {
+        val firstEmptyEdge = emptyEdges.head
+        val (row, col, _) = firstEmptyEdge
+        
+        // Hypothetical grid with a line ('l') in the first empty edge
+        val withGuess = Cell.setCell(stableGrid, (row, col, 'l'))
+        
+        // Try solving with the hypothetical line
+        solvePuzzle(withGuess, solveRules) match {
+          case Some(solution) => Some(solution)
+          case None => 
+          // The hypothetical line led to a contradiction, so place an 'x' instead
+          val withX = Cell.setCell(stableGrid, (row, col, 'x'))
+          solvePuzzle(withX, solveRules)
+        }
+      } else {
+        None
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val grids = PuzzleReaderWriter.readPuzzlesFromFile("puzzles.txt")
+
+    val startTime = System.nanoTime()  // Record the start time
+
+    val solveRules = RuleParser.getAllRulesFromFile("rules.txt")
+
+    for ((grid, index) <- grids.zipWithIndex) {
+      println(s"Solving puzzle ${index + 1}")
+      println(PuzzleReaderWriter.gridToString(grid) + "\n")
+
+      val puzzleStartTime = System.nanoTime()  // Record the start time for this puzzle
+
+      // Solve the puzzle
+      solvePuzzle(grid, solveRules) match {
+        case Some(solvedPuzzle) =>
+          println("Solved Puzzle:")
+          println(PuzzleReaderWriter.gridToString(solvedPuzzle))
+        case None =>
+          println("Puzzle is unsolvable")
+      }
+
+      val puzzleEndTime = System.nanoTime()  // Record the end time for this puzzle
+      val puzzleTimeElapsed = (puzzleEndTime - puzzleStartTime) / 1e6  // Time in milliseconds
+      println(s"Time taken to solve puzzle ${index + 1}: $puzzleTimeElapsed ms")
+
+      println("\n--------------------------\n")
+    }
+
+    val endTime = System.nanoTime()  // Record the end time
+    val timeElapsed = (endTime - startTime) / 1e6  // Time in milliseconds
+
+    println(s"Total time taken: $timeElapsed ms")
+  }
+
+}
