756908404f78710cbed5c388592e726df7c97444
Gabriel Løsnesløkken <gabriell@uia.no>
2023-09-26 13:59:15 +0200

    Changes try to fix

---
+++ DataAssignment/PuzzleReaderWriter.scala
@@ -0,0 +1,45 @@
+import java.io.{FileInputStream, FileOutputStream}
+import scala.io._
+import java.util._
+
+object PuzzleReaderWriter{
+//  var unsolvedFile:String="";
+//  var solvedFile:String="";
+//  var puzzles:List[Schema.Puzzle] = null;
+//  var num_puzzles:Int = 0;
+//  val output = Schema.Solutions.newBuilder();
+//  var outputfile: FileOutputStream = null;
+//
+//  def initRW(infile:String, outfile:String)={
+//    unsolvedFile=infile
+//	solvedFile=outfile
+//    val input = Schema.Puzzles.parseFrom(new FileInputStream(unsolvedFile));
+//    puzzles = input.getPuzzleList;
+//    num_puzzles = puzzles.size;
+//    outputfile = new FileOutputStream(solvedFile);
+//  }
+//
+//  def getNumPuzzles():Int={
+//    return num_puzzles;
+//  }
+//
+//  def getPuzzle(index:Int):Puzzle={
+//    val puzzle=puzzles.get(index);
+//	return new Puzzle(puzzle.getSizex(),puzzle.getSizey(),"")
+//  }
+//
+//  def putSolution(puzzle:Puzzle)={
+//    val puzzleOut=Schema.Solution.newBuilder().setSizex(puzzle.sizeX).setSizey(puzzle.sizeY).setSol(puzzle.solution).build();
+//	output.addSolution(puzzleOut);
+//  }
+//
+//  def closing()={
+//    output.build().writeTo(outputfile);
+//    outputfile.close();
+//  }
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+
+}
