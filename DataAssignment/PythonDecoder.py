4d18b1812b84748489df86e2065905939e4dc921
Gabriel Løsnesløkken <gabriell@uia.no>
2023-10-24 14:17:56 +0200

    Correct the Encoder.py

--- DataAssignment/PythonDecoder.py
+++ DataAssignment/PythonDecoder.py
@@ -1,30 +1,57 @@
 import sys
 import schema_pb2
 
+# Command-line arguments for the input and output files
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
 
+# Function to calculate grid coordinates based on a linear index and grid width
+def calculate_coordinates(index, size_x):
+    x = (index % size_x) * 2
+    y = (index // size_x) * 2
+    return x, y
+
+
+# Function to read from a binary file into a Protocol Buffers object
 def readFromFile():
-    puzzles = schema_pb2.Solutions()
+    solutions = schema_pb2.Solutions()
     with open(infile, "rb") as f:
-        puzzles.ParseFromString(f.read())
-    return puzzles
+        solutions.ParseFromString(f.read())
+    return solutions
 
 
+# Function to write Protocol Buffers object data to a text file
 def writeToFile(solutions):
-    count = 0
-    with open(outfile, "a") as f:
-        topic = "puzzles " + str(len(solutions.solution)) + "\n"
-        f.write(topic)
-    for puzzle in solutions.solution:
+    for solution in solutions.solution:
+        # Initialize grid based on solution data
+        grid = [[' ' for _ in range(solution.size_x * 2 - 1)] for _ in range(solution.size_y * 2 - 1)]
+
+        # Update grid based on hints and connections
+        for index in solution.white_hints:
+            x, y = calculate_coordinates(index, solution.size_x)
+            grid[y][x] = 'o'
+        for index in solution.black_hints:
+            x, y = calculate_coordinates(index, solution.size_x)
+            grid[y][x] = '*'
+        for connection in solution.connections:
+            x_from, y_from = calculate_coordinates(connection.index_from, solution.size_x)
+            x_to, y_to = calculate_coordinates(connection.index_to, solution.size_x)
+            x_between = (x_from + x_to) // 2
+            y_between = (y_from + y_to) // 2
+            grid[y_between][x_between] = 'l'
+
+        # Generate text_output from the updated grid
+        text_output = f"SizeX: {solution.size_x}\nSizeY: {solution.size_y}\nGrid:\n"
+        text_output += '\n'.join([' '.join(row) for row in grid])
+
+        # Write text_output to the output text file
         with open(outfile, "a") as f:
-            sizex = puzzle.sizex
-            sizey = puzzle.sizey
-            topic2 = "size " + str(sizex) + "x" + str(sizey) + "\n"
-            f.write(topic2)
-            f.write(puzzle.sol+"\n")
+            f.write(text_output + "\n\n")
 
 
-solutionsFromScala = readFromFile()
-writeToFile(solutionsFromScala)
+# Read binary data into Protocol Buffers object
+solutions = readFromFile()
+
+# Write Protocol Buffers object data to text file
+writeToFile(solutions)
--- DataAssignment/PythonEncoder.py
+++ DataAssignment/PythonEncoder.py
@@ -1,64 +1,42 @@
-# PythonEncoder.py
+import sys
 import schema_pb2
 
+# Default file paths for local execution
+default_infile = "puzzles_input.txt"
+default_outfile = "serialized_puzzles.pb"
 
-def calculate_coordinates(index, size_x):
-    x = (index % size_x) * 2
-    y = (index // size_x) * 2
-    return x, y
+# Use command-line arguments if provided, else use default files
+infile = sys.argv[1] if len(sys.argv) > 1 else default_infile
+outfile = sys.argv[2] if len(sys.argv) > 2 else default_outfile
 
 
-def encode_protobuf_to_text(binary_input, output_file):
-    # Deserialize the binary input into a Protocol Buffers message
-    solution: schema_pb2.Solution = schema_pb2.Solution()  # Replace with your specific message type
-    solution.ParseFromString(binary_input)
+# Reads the input and returns a list of puzzle sizes.
+def splitFile(inputFile):
+    with open(inputFile, "r") as file:
+        content = file.readlines()
+        content.pop(0)
+        boards = list()
 
-    grid = solution.Puzzle
+        for line_index, line in enumerate(content):
+            content[line_index] = line.rstrip('\n')
 
-    size_x = grid.sizeX
-    size_y = grid.sizeY
+        for line_index, line in enumerate(content):
+            if "size" in line:
+                size_x = int(line[4:line.rfind('x')])
+                size_y = int(line[line.rfind('x')+1:])
+                print(f"Found puzzle with size ({size_x}, {size_y})")
+                boards.append((size_x, size_y))
+        return boards
 
-    white_hints = grid.whiteHints
-    black_hints = grid.blackHints
 
-    # Create a (n*2-1)^2 sized grid based on the specified pattern:
-    # Every index where x and y is odd, it should insert 'v'.
-    # Every index where x and y is even, it should insert '.'
-    # Else insert ' '
-    grid = [[' ' if x % 2 == 0 or y % 2 == 0 else '.' if (x % 2 != 0) and (y % 2 != 0) else 'v' for x in range(size_x * 2 - 1)] for y in range(size_y * 2 - 1)]
+if __name__ == "__main__":
+    protoPuzzles = schema_pb2.Puzzles()
+    inputPuzzles = splitFile(infile)
 
-    # Temp Code: Printing the grid for visualization
-    for row in grid:
-        print(' '.join(row))
+    for puzzle in inputPuzzles:
+        protoPuzzle = protoPuzzles.puzzle.add()
+        protoPuzzle.sizeX = puzzle[0]
+        protoPuzzle.sizeY = puzzle[1]
 
-    # Fill the grid with 'W' for white hints and 'B' for black hints based on indices
-    for index in white_hints:
-        x, y = calculate_coordinates(index, size_x)
-        grid[y][x] = 'o'
-
-    for index in black_hints:
-        x, y = calculate_coordinates(index, size_x)
-        grid[y][x] = '*'
-
-    # Find the line connections position for each connection
-    for connection in solution.Connections:
-        index_from = connection.indexFrom
-        index_to = connection.indexTo
-
-        x_from, y_from = calculate_coordinates(index_from, size_x)
-        x_to, y_to = calculate_coordinates(index_to, size_x)
-
-        # Calculate the coordinate in between the two indices
-        x_between = (x_from + x_to) // 2
-        y_between = (y_from + y_to) // 2
-
-        grid[y_between][x_between] = 'l'
-
-    # Create the text format output
-    text_output = f"SizeX: {size_x}\nSizeY: {size_y}\nGrid:\n" + "\n".join([cell for row in grid for cell in row])
-
-    print(text_output)
-
-    # Write the binary data to the output file
-    with open(output_file, "wb") as file:
-        file.write(text_output)
+    with open(outfile, "wb") as f:
+        f.write(protoPuzzles.SerializeToString())
