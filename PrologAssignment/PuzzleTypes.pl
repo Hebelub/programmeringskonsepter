265ef88b61c4b674fd6de57cd4b39fb8a423411d
Gabriel Løsnesløkken <gabriell@uia.no>
2023-11-21 17:22:45 +0100

    Consistant naming and remove redundant functions

--- PrologAssignment/PuzzleTypes.pl
+++ PrologAssignment/PuzzleTypes.pl
@@ -1,28 +1,26 @@
 % FILEPATH: /C:/Users/glosn/uia/submission/gabriell/PrologAssignment/PuzzleTypes.pl
 
-:- include('Utility.pl').
-
 % Mapping the symbols to cell types
 cell_type('*', black).
 cell_type('o', white).
 cell_type('_', empty).
 
 % Cell type definitions
-validCell((white, Up, Right, Down, Left)) :- isStraight(Up, Right, Down, Left).
-validCell((black, Up, Right, Down, Left)) :- isTurn(Up, Right, Down, Left).
-validCell((empty, Up, Right, Down, Left)) :- validEmpty(Up, Right, Down, Left).
+valid_cell((white, Up, Right, Down, Left)) :- is_straight(Up, Right, Down, Left).
+valid_cell((black, Up, Right, Down, Left)) :- is_turn(Up, Right, Down, Left).
+valid_cell((empty, Up, Right, Down, Left)) :- valid_empty(Up, Right, Down, Left).
 
 % Valid white cell must be straight.
-isStraight(true, false, true, false).
-isStraight(false, true, false, true).
+is_straight(true, false, true, false).
+is_straight(false, true, false, true).
 
 % Valid black cell must be a turn.
-isTurn(true, true, false, false).
-isTurn(false, true, true, false).
-isTurn(false, false, true, true).
-isTurn(true, false, false, true).
+is_turn(true, true, false, false).
+is_turn(false, true, true, false).
+is_turn(false, false, true, true).
+is_turn(true, false, false, true).
 
 % Valid empty cell can either have two connections, no connections, or valid straight/turn configurations
-validEmpty(false, false, false, false). % No connections
-validEmpty(Up, Right, Down, Left) :- isStraight(Up, Right, Down, Left). % Straight configuration
-validEmpty(Up, Right, Down, Left) :- isTurn(Up, Right, Down, Left). % Turn configuration
+valid_empty(false, false, false, false). % No connections
+valid_empty(Up, Right, Down, Left) :- is_straight(Up, Right, Down, Left). % Straight configuration
+valid_empty(Up, Right, Down, Left) :- is_turn(Up, Right, Down, Left). % Turn configuration
