c4301cc91963b92baccb3c329d3410970eea5196
Gabriel Løsnesløkken <gabriell@uia.no>
2023-11-29 14:29:43 +0100

    New way of determining

--- PrologAssignment/PuzzleTypes.pl
+++ PrologAssignment/PuzzleTypes.pl
@@ -1,5 +1,6 @@
 % FILEPATH: /C:/Users/glosn/uia/submission/gabriell/PrologAssignment/PuzzleTypes.pl
 
+:- include('PrintStar.pl').
 
 % Checking Cells
 
@@ -47,3 +48,51 @@ valid_star(star((black, true, false, false, true), (_ true, false, true, false),
 
 % Define a valid star cell with empty center
 valid_star(star((empty, _, _, _, _), _, _, _, _)).
+
+
+
+% Determine the center cell's connections based on its type and adjacent cells
+determine_center_connections(Star, CenterCell) :-
+    % writeln('Determening for Star: '), print_star(Star), nl.
+
+    % Extract the type of the center cell and adjacent cells from the star
+    Star = star((Type, _, _, _, _), AdjUp, AdjRight, AdjDown, AdjLeft),
+
+    % Try all possible combinations of connections for the center cell
+    member(Up, [true, false]),
+    member(Right, [true, false]),
+    member(Down, [true, false]),
+    member(Left, [true, false]),
+
+    % Check if the star configuration is valid with the given connections
+    valid_star(star((Type, Up, Right, Down, Left), AdjUp, AdjRight, AdjDown, AdjLeft)),
+
+    % Return the center cell with determined connections
+    CenterCell = (Type, Up, Right, Down, Left).
+
+
+
+% TESTING TO GENERATE STARS
+
+% Generate and print all valid star combinations
+generate_and_print_all_valid_stars :-
+    findall(Star, generate_valid_star(Star), Stars),
+    maplist(print_star, Stars).
+
+% Generate a valid star configuration
+generate_valid_star(Star) :-
+    Star = star(Center, AdjUp, AdjRight, AdjDown, AdjLeft),
+    % Ensure the center and adjacent cells have matching connections
+    Center = (_, Up, Right, Down, Left),
+    AdjUp = (_, _, _, Up, _),
+    AdjRight = (_, _, _, _, Right),
+    AdjDown = (_, Down, _, _, _),
+    AdjLeft = (_, _, Left, _, _),
+    % Ensure each cell is valid
+    valid_cell(Center),
+    valid_cell(AdjUp),
+    valid_cell(AdjRight),
+    valid_cell(AdjDown),
+    valid_cell(AdjLeft),
+    % Check if the star configuration is valid
+    valid_star(Star).
